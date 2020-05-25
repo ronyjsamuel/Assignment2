d0a4f6fd38df31f25c01de0efb9ab334f959e6df
ronyjsamuel <ronysunrise@gmail.com>
2020-05-25 15:13:06 +0530

    react-fragments

---
+++ React-Fragment/script.js
@@ -0,0 +1,18 @@
+const element = <ul>
+  <React.Fragment>
+    Some text.
+    <h2>A heading</h2>
+    More text.
+    <h2>Another heading</h2>
+    Even more text.
+</React.Fragment>
+</ul>;
+
+
+ReactDOM.render(
+  element,
+  document.getElementById("root")
+)
+
+
+
